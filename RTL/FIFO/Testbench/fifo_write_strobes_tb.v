c9e1f3b5e52a35d2168d4715e559fe78a33c08a7
mike taylor <mike@dewberryfields.com>
2022-11-20 19:16:36 +0000

    fixed FIFO write strob test bench

--- RTL/FIFO/Testbench/fifo_write_strobes_tb.v
+++ RTL/FIFO/Testbench/fifo_write_strobes_tb.v
@@ -18,6 +18,7 @@
 // You should have received a copy of the GNU General Public License
 // along with dogtag.  If not, see <http://www.gnu.org/licenses/>.
  */
+ `include "RTL/FIFO/fifo_write_strobes.v"
 
 module fifo_write_strobes_tb; 
 
