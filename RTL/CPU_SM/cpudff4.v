68f0280a361e809658d4b1827788e39f0c89b4ec
mbtaylor1982 <mike@dewberryfields.com>
2023-01-15 18:37:52 +0000

    correcting mistake in cpu state machine equ

--- RTL/CPU_SM/cpudff4.v
+++ RTL/CPU_SM/cpudff4.v
@@ -32,9 +32,44 @@ module cpudff4 (
 );
 wire p4a, p4b, p4c;
 
-assign p4a = (~(~(~E61 & ~E60 & ~E2 & ~E3 & ~E5 & ~E7 & ~E12 & ~E8) | ~(~E55 & ~E18 & ~E19 & ~E48 & ~E21 & ~E31 & ~E34 & ~E45)) & ~(~(~E50_d_E52_d & ~E9_d & ~E25_d & ~E28_d & ~E30_d) & DSACK));
-assign p4b = (~(~STERM_ & (~(~E51_s_E54_sd & ~E46_s_E59_s & E36_s_E47_s) | ~(E33_sd_E38_s & ~E39_s & ~E40_s_E41_s) | ~(~E42_s & ~E43_s_E49_sd & ~E37_s_E44_s))));
-assign p4c = (~((~(~(E23_sd & DSACK) & ~(~DSACK & (E51_s_E54_sd | E43_s_E49_sd))) | (E57_s | E46_s_E59_s))& STERM_));
+assign p4a = 
+(
+  ~(
+    ~(~E61 & ~E60 & ~E2 & ~E3 & ~E5 & ~E7 & ~E12 & ~E8) | 
+    ~(~E55 & ~E18 & ~E19 & ~E48 & ~E21 & ~E31 & ~E34 & ~E45)
+  ) & 
+  ~(
+    ~(~E50_d_E52_d & ~E9_d & ~E25_d & ~E28_d & ~E30_d) 
+    & DSACK
+  )
+);
+
+assign p4b = 
+(
+  ~(~STERM_ & 
+      (
+        ~(~E51_s_E54_sd & ~E46_s_E59_s & ~E36_s_E47_s) | 
+        ~(~E33_sd_E38_s & ~E39_s & ~E40_s_E41_s) | 
+        ~(~E42_s & ~E43_s_E49_sd & ~E37_s_E44_s)
+      )
+  )
+);
+
+assign p4c = 
+(
+  ~(
+    (
+      ~(
+        ~(E23_sd & DSACK) & 
+        ~(~DSACK & 
+          (E51_s_E54_sd | E43_s_E49_sd)
+        )
+      ) | 
+      (E57_s | E46_s_E59_s)
+    )& 
+    STERM_
+  )
+);
 
 assign cpudff4_d = (~(p4a & p4b & p4c));
 
