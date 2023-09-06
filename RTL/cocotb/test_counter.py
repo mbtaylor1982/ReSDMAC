caeda0ba508d0e7b50dee7b30afde9ef53b6788e
mbtaylor1982 <mike@dewberryfields.com>
2023-09-06 12:29:00 +0100

    fixtest

--- RTL/cocotb/test_counter.py
+++ RTL/cocotb/test_counter.py
@@ -8,7 +8,7 @@ hdl_dir = os.path.dirname(__file__)
 def test_ounter():
     run(
         toplevel_lang="verilog",
-        verilog_sources=[os.path.join("./../FIFO/", "fifo_3bit_cntr.v")],
+        verilog_sources=[os.path.join("RTL/FIFO/", "fifo_3bit_cntr.v")],
         toplevel="fifo_3bit_cntr",            # top level HDL
         module="cocotb_fifo_3bit_cntr",        # name of cocotb test module
         python_search=[hdl_dir],
